37c868e9370d5c90befad36a55d97d994ce8efc4
Fernanda de Melo Reis <114718084+fernandamelreis@users.noreply.github.com>
2023-05-29 17:21:57 -0300

    Update jogo_pedra_papel_tesoura.py
    Atualizando código com a biblioteca emojize

--- jogo_pedra_papel_tesoura.py
+++ jogo_pedra_papel_tesoura.py
@@ -1,13 +1,21 @@
 import random
+from emoji import emojize
+from time import sleep
 
 user_points = 0
 computer_points = 0
 
 options = ['r', 't', 'p'] #lista
 
+print("~~" * 30)
+print(emojize(":scissors:  JOGO PEDRA, PAPEL, TESOURA"))
+print("~~" * 30)
+sleep(1)
+
+
 while True:
 
-    user_choice = input("Escolha R para Pedra / T para Tesoura / P para Papel ou Q para SAIR:_ ").lower() #transforma a entrada do usuário em minúsculo
+    user_choice = input(emojize(":scissors:  Escolha R para Pedra / T para Tesoura / P para Papel ou Q para SAIR:_ ")).lower() #transforma a entrada do usuário em minúsculo
     if(user_choice == 'q'):
         break
 
@@ -18,47 +26,47 @@ while True:
     # 0 = R - PEDRA / 1 = T- TESOURA / 2 = P-PAPEL
     computer_option = options[computer_choice]
 
-    print("O computador escolheu " + str(computer_option))
+    print(emojize(":robot: O computador escolheu ") + str(computer_option))
 
     if user_choice == 't' and computer_choice == 't':
-        print("Empate!")
+        print(emojize(":robot: Empate!"))
     elif user_choice == 'r' and computer_choice == 'r':
-        print("Empate!")
+        print(emojize(":robot: Empate!"))
     elif user_choice == 'p' and computer_choice == 'p':
-        print("Empate!")
+        print(emojize(":robot: Empate!"))
     elif (user_choice == 'r') and (computer_choice == 't'):
-        print("Usuário ganhou!")
+        print(emojize(":heart: Usuário ganhou!"))
         user_points = user_points + 1
         computer_points = computer_points + 0
     elif (user_choice == 'p') and (computer_choice == 'r'):
-        print("Usuário ganhou!")
+        print(emojize(":heart: Usuário ganhou!"))
         user_points = user_points + 1
         computer_points = computer_points + 0
     elif (user_choice == 't') and (computer_choice == 'p'):
-        print("Usuário ganhou!")
+        print(emojize(":heart: Usuário ganhou!"))
         user_points = user_points + 1
         computer_points = computer_points + 0
     elif (computer_choice == 't') and (user_choice == 'p'):
-        print("Computador ganhou!")
+        print(emojize(":robot: Computador ganhou!"))
         computer_points = computer_points + 1
     elif (computer_choice == 'p') and (user_choice == 'r'):
-        print("Computador ganhou!")
+        print(emojize(":robot: Computador ganhou!"))
         computer_points = computer_points + 1
     elif (computer_choice == 'r') and (user_choice == 'p'):
-        print("Computador ganhou!")
+        print(emojize(":robot: Computador ganhou!"))
         computer_points = computer_points + 1
     else:
-        print("Você perdeu!")
+        print(emojize(":robot: Você perdeu!"))
         computer_points = computer_points + 1
 
     print("-------------------")
     print("Sua pontuação é: " + str(user_points))
     print("Pontuação do computador: " + str(computer_points))
 if computer_points > user_points:
-    print("Derrota!!!")
+    print(emojize(":robot: Derrota!"))
 elif computer_points == user_points:
-    print("Empate")
+    print(emojize(":robot: Empate"))
 else:
-    print("Vitória!")
+    print(emojize(":heart: Vitória!"))
 
-print("Goodbye my friend!")
+print(emojize(":banana: Goodbye my friend!"))
